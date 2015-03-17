4a5726a335d4968f8b5ddd20fa8ce711317e3c42
xiwc <xiwc87@yeah.net>
2015-03-17 17:52:03 +0800

    separate spring boot with spring

--- src/main/java/com/emc/pie/esa/test/spring/AppConfig.java
+++ src/main/java/com/emc/pie/esa/test/spring/AppConfig.java
@@ -2,14 +2,12 @@ package com.emc.pie.esa.test.spring;
 
 import java.util.Date;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@EnableAutoConfiguration
 @ComponentScan
 @PropertySource(value = "classpath:application.properties")
 public class AppConfig {
--- src/main/java/com/emc/pie/esa/test/spring/boot/App.java
+++ src/main/java/com/emc/pie/esa/test/spring/boot/App.java
@@ -3,14 +3,18 @@ package com.emc.pie.esa.test.spring.boot;
 import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 import com.emc.pie.esa.test.spring.AppConfig;
 
+@SpringBootApplication
+@Import(AppConfig.class)
 public class App {
 
 	public static void main(String[] args) throws IOException {
 
-		SpringApplication.run(AppConfig.class, args);
+		SpringApplication.run(App.class, args);
 
 		System.out.println("Spring application starting success.");
 	}
