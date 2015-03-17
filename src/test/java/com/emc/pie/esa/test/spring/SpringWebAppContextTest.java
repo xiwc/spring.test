b4fe7bcf0d13b0b2baa93d5c90771631dd609c3c
xiwc <xiwc87@yeah.net>
2015-03-17 15:56:22 +0800

    log framework handle
    1. add logback.xml
    2. remove log4j.properties

--- src/test/java/com/emc/pie/esa/test/spring/SpringWebAppContextTest.java
+++ src/test/java/com/emc/pie/esa/test/spring/SpringWebAppContextTest.java
@@ -1,55 +1,48 @@
 package com.emc.pie.esa.test.spring;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { AppConfig.class })
+// @SpringApplicationConfiguration(classes = { AppConfig.class })
+@ContextConfiguration(classes = { AppConfig.class })
 @WebAppConfiguration
+@TestPropertySource("/spring.beans.properties")
 public class SpringWebAppContextTest {
+
+	static Logger logger = Logger.getLogger(SpringWebAppContextTest.class);
 
 	@Autowired
 	ApplicationContext ctx;
 
+	@Autowired
+	Environment env;
+
 	@Test
 	public void printMgrBeans() throws Exception {
 
-		String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+		String[] names = ctx.getBeanDefinitionNames();
 
-		Arrays.sort(beanDefinitionNames);
+		Arrays.sort(names);
 
-		System.out.println(beanDefinitionNames.length);
+		logger.debug(names.length);
+		logger.info(names.length);
 
-		InputStream resourceAsStream = Thread.currentThread().getClass()
-				.getResourceAsStream("/spring.beans.properties");
+		Arrays.stream(names).forEach((name) -> {
+			System.out.println(String.format("%s=%s", name, ctx.getType(name).toString()));
+		});
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream, "utf-8"));
+		org.junit.Assert.assertTrue(env.containsProperty("actuatorMetricRepository"));
 
-		List<String> lines = new ArrayList<String>();
-
-		String line = null;
-
-		while ((line = reader.readLine()) != null) {
-			lines.add(line);
-		}
-
-		for (String name : beanDefinitionNames) {
-			if (!lines.contains(name)) {
-				System.out.println(name);
-				System.out.println(ctx.getType(name).toString());
-			}
-		}
 	}
 }
